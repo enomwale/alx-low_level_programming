184f118ec3c4a6be3ee2952bcea22b98501b16e2
enomwale <mwaleenock2001@gmail.com>
2023-06-18 16:53:40 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
+ * main - Entry point
  * Return 1
  */
 int main(void)
 {
-fput("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", stdout);
+fput("and that place of art is useful\" - Dora Korpar, 2015-10-1\n", stdout);
 return (1);
 }
