f76d214de7a117100d1fa18a7403b840b713620a
enomwale <mwaleenock2001@gmail.com>
2023-06-21 06:44:19 -0700

    Task 2 This program prints the largest of three integers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ if (a > b && b > c)
 {
 largest = a;
 }
-else if (b < a && a < c)
-{
-largest = c;
-}
-else if (b > c)
+else if (b > a && a > c)
 {
 largest = b;
+}
+else if (c > b)
+{
+largest = c;
 }
 else
 {
