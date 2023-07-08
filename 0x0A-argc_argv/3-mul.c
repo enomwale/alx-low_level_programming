36fa41df2172583b09ea5d69da79ab66e194e5c9
enomwale <mwaleenock2001@gmail.com>
2023-07-08 15:57:22 -0700

    Task 3 - Write a program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - creates an integer from a string
+ * @s: string to be changed
+ * Return: the string's conversion to int
+ */
+int _atoi(char *s)
+{
+int j, a, n, len, f, digit;
+j = 0;
+a = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (j < len && f == 0)
+{
+if (s[j] == '-')
+++a;
+if (s[j] >= '0' && s[j] <= '9')
+{
+digit = s[j] - '0';
+if (a % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[j + 1] < '0' || s[j + 1] > '9')
+break;
+f = 0;
+}
+j++;
+}
+if (f == 0)
+return (0);
+return (n);
+}
+/**
+ * main - adds two numbers together
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+int result, sum1, sum2;
+if (argc < 3 || argc > 3)
+{
+printf("Error\n");
+return (1);
+}
+sum1 = _atoi(argv[1]);
+sum2 = _atoi(argv[2]);
+result = sum1 *sum2;
+printf("%d\n", result);
+return (0);
