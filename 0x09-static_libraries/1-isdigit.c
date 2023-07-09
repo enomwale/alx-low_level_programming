e6aace15a563ec8fbdc34c14cfdf8060fe423025
enomwale <mwaleenock2001@gmail.com>
2023-07-09 15:04:57 -0700

    1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - verifies each digit from 0 to 9
+ * @c: int to be examined
+ * Return: 0 otherwise, 1 if c is a digit
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
