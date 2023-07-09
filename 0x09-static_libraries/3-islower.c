3bc46209ef33d79664505c44ce5772a14d9a3990
enomwale <mwaleenock2001@gmail.com>
2023-07-09 15:54:01 -0700

    3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for small character
+ * @c: the character to check
+ * Return: 1 if c is small, 0 otherwise
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
