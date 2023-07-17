ebe6ff982556ab9b812fb82f26025ad98b85d48d
enomwale <mwaleenock2001@gmail.com>
2023-07-17 13:15:26 -0700

    Task 1 Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,17 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * PI - as an abbreviation for the token 3.14159265359
+ * Return: Always 0.
+ */
+int main(void)
+{
+float a;
+float r;
+a = pi * r * r;
+r = 98;
+printf("%.3f\n", a);
+return (0);
+}
